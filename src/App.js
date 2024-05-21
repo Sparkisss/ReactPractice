21922b3aaf9665472e90a278ec6929e78f456e73
Igor Siamykin <sparkisisss@gmail.com>
2024-05-21 16:07:58 +0300

    21-05-24

--- src/App.js
+++ src/App.js
@@ -1,75 +1,23 @@
-import React, {useState, useMemo, useEffect} from 'react';
-import Counter from './components/counter/Counter';
-import './components/styles/style.css'
-import PostList from './components/postList/PostList';
-import PostForm from './components/postForm/PostForm';
-import PostFilter from './components/postFilter/PostFilter';
-import MyModal from './components/UI/modal/MyModal';
-import MyButton from './components/UI/button/MyButton';
-import { usePosts } from './hooks/usePosts';
-import PostService from './API/PostService';
-import Loader from './components/UI/loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import {getPageCount} from './utils/pages'
+import React from 'react';
+import './components/styles/style.css';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import About from './pages/About';
+import Posts from './pages/Posts'
+import Navbar from './components/UI/navbar/Navbar';
+import NotFound from './pages/NotFound';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  let pagesArray = []
-  for (let i = 0; i < totalPages; i++) {
-    pagesArray.push(i + 1)
-  }
-
-  console.log([pagesArray])
-
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async() => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data)
-    const totalCount = response.headers['x-total-count'];
-    setTotalPages(getPageCount(totalCount, limit));
-  })
-
-  useEffect(() => {
-    fetchPosts();
-  }, [])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
   return (
-    <div className='App'>      
-      <Counter/>
-      <button onClick={fetchPosts}>Response</button>
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
-        Add user
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost}/>
-      </MyModal>      
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter filter={filter} setFilter={setFilter}/>
-      {postError &&
-        <h2>Error occurred ${postError}</h2>
-      }
-      {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div> 
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Evgen'}/>
-      }
-      
-    </div>
+    <Router>
+      <Navbar/>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='*' element={<NotFound />} /> {/* Обработка несуществующих маршрутов */}
+        </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/postItem/PostItem.jsx
+++ src/components/postItem/PostItem.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import MyButton from '../UI/button/MyButton';
+import { useNavigate } from 'react-router-dom';
 
 const PostItem = (props) => {
+    const navigate = useNavigate();
+    console.log(navigate)
+
     return (
         <div className='wrapper'>
         <div className='contain'>
-            <h2>{props.number} {props.post.title}</h2>
+            <h2>{props.post.id} {props.post.title}</h2>
             <div>
                 <p>{props.post.body}</p>
             </div>
+            <MyButton onClick={() => navigate.push(`/posts/${props.post.id}`)}>
+                Open
+            </MyButton>
             <MyButton onClick={() => props.remove(props.post)}>
                 Delete
             </MyButton>
